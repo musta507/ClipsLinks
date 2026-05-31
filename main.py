55b98f3bf129c39dc0e00068e68f1015ec406453
musta507 <musta.gamer507@gmail.com>
2026-05-31 07:08:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, send_from_directory
 from flask_cors import CORS
 import yt_dlp
+import os
 
 app = Flask(__name__)
 CORS(app)
+
+@app.route('/')
+def index():
+    return send_from_directory('.', 'index.html')
 
 @app.route('/links')
 def get_links():
